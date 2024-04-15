d8301c30493a223b33b31dec46809ed161c20377
Wen Hsuan-che <160596981+Alexwen920520@users.noreply.github.com>
2024-04-15 13:43:18 +0000

    123412341234

--- 20240409/好難.py
+++ 20240409/好難.py
@@ -16,12 +16,12 @@ m = folium.Map(location=[locations[0][0], locations[0][1]], zoom_start=13)
 
 # 使用從DataFrame中提取的經緯度數據創建Marker並添加到地圖上
 for i, location in enumerate(locations):
-    popup_text = f"地震時間：{times[i]}<br>" \
-                 f"位置：北緯 {location[0]}度，東經 {location[1]}度<br>" \
-                 f"即在{places[i]}<br>" \
-                 f"地震深度：{depths[i]}公里<br>" \
-                 f"芮氏規模：{magnitudes[i]}"
-    folium.Marker(location, popup=folium.Popup(popup_text, max_width=300), color="red", fill=True, fill_color="red").add_to(m)
+    tooltip_text = f"地震時間：{times[i]}<br>" \
+                   f"位置：北緯 {location[0]}度，東經 {location[1]}度<br>" \
+                   f"即在{places[i]}<br>" \
+                   f"地震深度：{depths[i]}公里<br>" \
+                   f"芮氏規模：{magnitudes[i]}"
+    folium.Marker(location, tooltip=tooltip_text, color="red", fill=True, fill_color="red").add_to(m)
 
 # 保存地圖到指定的工作區
 m.save('/workspaces/cycu_ai2024/20240409/earthquake_map.html')
