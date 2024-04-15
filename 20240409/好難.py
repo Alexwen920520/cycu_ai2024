26977318ef5e4d3b9b83aa923e9219a86ad89065
Wen Hsuan-che <160596981+Alexwen920520@users.noreply.github.com>
2024-04-15 13:29:30 +0000

    1231231

--- 20240409/好難.py
+++ 20240409/好難.py
@@ -6,13 +6,17 @@ df = pd.read_csv('/workspaces/cycu_ai2024/20240409/地震活動彙整_6384880576
 
 # 提取地震位置的經緯度
 locations = df[['緯度', '經度']].values
+magnitudes = df['規模'].values
+depths = df['深度'].values
+places = df['位置'].values
 
 # 創建一個新的folium地圖實例
 m = folium.Map(location=[locations[0][0], locations[0][1]], zoom_start=13)
 
-# 使用從DataFrame中提取的經緯度數據創建Polygon並添加到地圖上
-for location in locations:
-    folium.Polygon([location], color="red", fill=True, fill_color="red").add_to(m)
+# 使用從DataFrame中提取的經緯度數據創建Marker並添加到地圖上
+for i, location in enumerate(locations):
+    popup_text = f"規模: {magnitudes[i]}, 深度: {depths[i]}, 位置: {places[i]}"
+    folium.Marker(location, popup=popup_text, color="red", fill=True, fill_color="red").add_to(m)
 
 # 保存地圖到指定的工作區
 m.save('/workspaces/cycu_ai2024/20240409/earthquake_map.html')
